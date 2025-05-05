99e5e14c32748c83fdb77d4f23e2835a6d479581
Nokita Kaze <admin@kanaria.ru>
2025-05-06 02:22:00 +0300

    + Issue #2 (load old history messages from discord) completed + Issue #3 (appeals) completed * Fixed GPT_DEFAULT_SYSTEM_ROLE and * The OpenAI package version has been upgraded to 4.97.0

--- main.js
+++ main.js
@@ -17,26 +17,63 @@ const GPT_PROMPT = process.env.GPT_PROMPT ?? "You are a helpful assistant. Respo
 
 let GPT_DEFAULT_SYSTEM_ROLE;
 {
-    const no_system_role = [
+    const no_developer_messages = [
         'o1-mini',
         'o1-preview',
     ];
+    const force_developer_models = [
+        'o1',
+        'o3',
+        'o4',
+    ];
 
-    let is_no_system_role = false;
+    let is_system_is_developer = false;
+    let is_no_developer = false;
     let lowered = GPT_MODEL.toLowerCase();
-    for (let prefix of no_system_role) {
+
+    for (let prefix of no_developer_messages) {
         if ((lowered === prefix) || lowered.startsWith(prefix + '-')) {
-            is_no_system_role = true;
+            is_no_developer = true;
             break;
         }
     }
 
-    GPT_DEFAULT_SYSTEM_ROLE = is_no_system_role ? 'user' : 'system';
+    if (!is_no_developer) {
+        for (let prefix of force_developer_models) {
+            if ((lowered === prefix) || lowered.startsWith(prefix + '-')) {
+                is_system_is_developer = true;
+                break;
+            }
+        }
+    }
+
+    if (is_no_developer) {
+        GPT_DEFAULT_SYSTEM_ROLE = 'user';
+    } else if (is_system_is_developer) {
+        GPT_DEFAULT_SYSTEM_ROLE = 'developer';
+    } else {
+        GPT_DEFAULT_SYSTEM_ROLE = 'system';
+    }
+}
+
+let GPT_DEFAULT_NO_CHAT_COMPLETION_API = false;
+{
+    const no_chat_completion_api_prefixes = [
+        'o1-pro',
+    ];
+    let lowered = GPT_MODEL.toLowerCase();
+    for (let prefix of no_chat_completion_api_prefixes) {
+        if ((lowered === prefix) || lowered.startsWith(prefix + '-')) {
+            GPT_DEFAULT_NO_CHAT_COMPLETION_API = true;
+            break;
+        }
+    }
 }
 
 const GPT_SYSTEM_ROLE = process.env.GPT_SYSTEM_ROLE ?? GPT_DEFAULT_SYSTEM_ROLE;
+const GPT_NO_CHAT_COMPLETION_API = process.env.GPT_NO_CHAT_COMPLETION_API ?? GPT_DEFAULT_NO_CHAT_COMPLETION_API;
 
-const openai = new OpenAI({
+const openaiClient = new OpenAI({
     apiKey: OPENAI_API_KEY,
 });
 
@@ -46,6 +83,7 @@ const unusefulMessagesLifetime = 24 * 3600 * 1000;
 let botName;
 const thisBotMessages = new Set();
 const allMessages = {};
+const usernames = {};
 let needShutdown = false;
 let currentProcessingMessaged = 0;
 
@@ -84,6 +122,7 @@ discordClient.on('messageCreate', async (/** @type {Message} */ message) => {
         return;
     }
 
+    usernames[message.author.id] = message.author.username;
     allMessages[message.id] = {
         id: message.id,
         referenceMessageId: message.reference?.messageId ?? null,
@@ -94,20 +133,24 @@ discordClient.on('messageCreate', async (/** @type {Message} */ message) => {
 
     if (message.author.bot) {
         // Author is a bot itself
+        if (message.author.id === discordClient.user.id) {
+            thisBotMessages.add(message.id);
+        }
+
         return;
     }
 
     const hasMentionMe = message.mentions.users.has(discordClient.user.id);
-    const answerToMe = (message.reference && thisBotMessages.has(message.reference.messageId));
+    const answerToMe = ((message.reference !== null) && thisBotMessages.has(message.reference.messageId));
     if (!hasMentionMe && !answerToMe) {
         // I can't answer to this message
         return;
     }
 
-    console.log('At ', new Date(message.createdTimestamp), '. Message from user ', message.author.username,
+    console.log('At ', new Date(message.createdTimestamp), '. Message from user ', message.author.displayName,
         '. Text: ', message.content)
     currentProcessingMessaged++;
-    const response = await generateResponse(message.id, OPENAI_API_KEY);
+    const response = await generateResponse(message.id, message.channelId, OPENAI_API_KEY);
     // const newMessage = await message.channel.send(response);
     if (response.length <= 1950) {
         const newMessage = await message.reply(response);
@@ -127,51 +170,13 @@ discordClient.on('messageCreate', async (/** @type {Message} */ message) => {
     currentProcessingMessaged--;
 });
 
-async function generateResponse(messageId) {
+async function generateResponse(messageId, channelId) {
     try {
-        const dialog = [];
-        let lastChainId = messageId;
-        while (true) {
-            if (!allMessages.hasOwnProperty(lastChainId)) {
-                break;
-            }
-
-            const message = allMessages[lastChainId];
-            if (thisBotMessages.has(lastChainId)) {
-                // This is THIS bot message
-                dialog.push({
-                    role: "assistant",
-                    content: message.text,
-                    name: botName,
-                });
-            } else {
-                // This is a user message
-                dialog.push({
-                    role: "user",
-                    content: message.text,
-                    name: message.authorName,
-                });
-            }
-
-            if (message.referenceMessageId === null) {
-                break;
-            }
-
-            lastChainId = message.referenceMessageId;
+        if (!GPT_NO_CHAT_COMPLETION_API) {
+            return await generateResponse_chat(messageId, channelId);
         }
-        dialog.push({role: GPT_SYSTEM_ROLE, content: [{type: "text", text: GPT_PROMPT}]});
 
-        /** https://platform.openai.com/docs/guides/text-generation/chat-completions-api */
-        /** https://platform.openai.com/docs/api-reference/chat/create */
-        const response = await openai.chat.completions.create({
-            model: GPT_MODEL,
-            messages: [...dialog].reverse(),
-            max_completion_tokens: 4096,
-            n: 1
-        });
-
-        console.log('OpenAI response. Model', response.model, '. Text: ', response.choices[0].message.content);
-        return response.choices[0].message.content;
+        return `Chat completion API is disabled for model ${GPT_MODEL}`;
     } catch (error) {
         console.error('Error generating response:', error.response ? error.response.data : error);
         if (error.error) {
@@ -180,6 +185,103 @@ async function generateResponse(messageId) {
             return `Sorry, I am unable to generate a response at this time. ${error}`;
         }
     }
+}
+
+async function generateResponse_chat(messageId, channelId) {
+    const dialog = [];
+    let lastChainId = messageId;
+    const channel = await discordClient.channels.fetch(channelId);
+    while (true) {
+        if (!allMessages.hasOwnProperty(lastChainId)) {
+            // no parent message loaded
+            const message1 = await channel.messages.fetch(lastChainId).catch(() => null);
+            if (!message1) {
+                // Can't get this message. Break the cycle
+                break;
+            }
+
+            usernames[message1.author.id] = message1.author.username;
+            allMessages[lastChainId] = {
+                id: message1.id,
+                referenceMessageId: message1.reference?.messageId ?? null,
+                time: message1.createdTimestamp,
+                authorName: message1.author.username,
+                text: message1.content,
+            };
+        }
+
+        const message = allMessages[lastChainId];
+        if (thisBotMessages.has(lastChainId)) {
+            // This is THIS bot message
+            dialog.push({
+                role: "assistant",
+                content: message.text,
+                name: botName,
+            });
+        } else {
+            // This is a user message
+            dialog.push({
+                role: "user",
+                content: message.text,
+                name: message.authorName,
+            });
+        }
+
+        if (message.referenceMessageId === null) {
+            // No parent message. It's the root message for this branch
+            break;
+        }
+
+        lastChainId = message.referenceMessageId;
+    }
+
+    // Processing dialog
+    for (let i = 0; i < dialog.length; i++) {
+        const messageItem = dialog[i];
+        const message = messageItem.content;
+        const fixedMessage = fixMessageAppeals(message);
+        if (message === fixedMessage) {
+            continue;
+        }
+
+        messageItem.content = fixedMessage;
+        dialog[i] = messageItem;
+    }
+
+    dialog.push({role: GPT_SYSTEM_ROLE, content: [{type: "text", text: GPT_PROMPT}]});
+
+    /** https://platform.openai.com/docs/guides/text?api-mode=chat */
+    /** https://platform.openai.com/docs/api-reference/chat/create */
+    const response = await openaiClient.chat.completions.create({
+        model: GPT_MODEL,
+        messages: [...dialog].reverse(),
+        max_completion_tokens: 4096,
+        n: 1
+    });
+
+    console.log('OpenAI response. Model', response.model, '. Text: ', response.choices[0].message.content);
+    return response.choices[0].message.content;
+}
+
+function fixMessageAppeals(msg) {
+    const m = msg.match(/^<@(\d+)>/);
+    if (!m) {
+        return msg;
+    }
+
+    const id = m[1];
+    let rest = msg.slice(m[0].length).trimStart();
+
+    if (id === discordClient.user.id) {
+        return rest;
+    }
+
+    if (usernames[id]) {
+        rest = rest.replace(/^[ ,]+/, '').trimStart();
+        return `${usernames[id]}, ${rest}`;
+    }
+
+    return msg;
 }
 
 /**
